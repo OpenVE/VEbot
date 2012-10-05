ebd204f1042607da3efe24942e7700520f084ed4
Daniel Rodríguez <sadasant@gmail.com>
2012-10-05 12:27:40 -0430

    Testing http Get and time.Tick, next: submodules

--- VEbot.go
+++ VEbot.go
@@ -3,6 +3,8 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
+	"time"
 	irc "github.com/fluffle/goirc/client"
 	"io/ioutil"
 )
@@ -16,21 +18,27 @@ type Config struct {
 
 var config Config
 
-func main() {
-	file, err := ioutil.ReadFile("./config.json")
+func readConfig(filename string) {
+	defer println("Reading Config at: " + filename)
+	file, err := ioutil.ReadFile(filename)
 	if err != nil {
-		fmt.Println(err)
+		println(err)
+		return
 	}
 	err = json.Unmarshal(file, &config)
 	if err != nil {
 		println(err)
 		return
 	}
+}
+
+func main() {
+	readConfig("./config.json")
 	client := irc.SimpleClient(config.Nick)
 	client.AddHandler("connected", connected)
 	client.AddHandler("disconnected", disconnected)
 	quit := make(chan bool)
-	err = client.Connect(config.Server)
+	err := client.Connect(config.Server)
 	if err != nil {
 		fmt.Printf("Connection Error: %s\n", err)
 	}
@@ -38,11 +46,28 @@ func main() {
 }
 
 func connected(conn *irc.Conn, line *irc.Line) {
-	conn.Pass(config.Password)
-	conn.Join(config.Channel)
-	conn.Privmsg(config.Channel, "Chévere")
+	println("Connected!")
+	//conn.Pass(config.Password)
+	//conn.Join(config.Channel)
+	//conn.Privmsg(config.Channel, "Chévere")
+	readTwitter(conn, line)
 }
 
 func disconnected(conn *irc.Conn, line *irc.Line) {
 	println("Disconnected")
+}
+
+func readTwitter(conn *irc.Conn, line *irc.Line) {
+	// Twitter Fetch
+	resp, err := http.Get("http://api.twitter.com/1/statuses/user_timeline.json?screen_name=OpenVE")
+	if err != nil {
+		println(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	fmt.Printf("%v\n", string(body))
+	c := time.Tick(1 * time.Minute)
+	for now := range c {
+		fmt.Printf("%v\n", now, resp)
+	}
 }
